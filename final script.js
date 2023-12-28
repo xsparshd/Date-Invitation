d39c500f4f4d496275fbf5c629b426f09792d76d
xsparshd <155073100+xsparshd@users.noreply.github.com>
2023-12-28 17:54:41 +0530

    Add files via upload

---
+++ final script.js
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Invitation to Kanak</title>
+  <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+  <div class="container">
+    <h1>Hi Kanak,</h1>
+    <p>As a mechanical enthusiast, I've crafted this page just like assembling an engine! How about assembling a date together? Interested?</p>
+    <button id="yesButton">Sure, let's assemble!</button>
+    <button id="noButton">Sorry, not this time.</button>
+  </div>
+
+  <script src="script.js"></script>
+</body>
+</html>
